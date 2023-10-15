3749614b10d3b03235683c68e8fd1ce2d3d94945
Agostinho Dalte Baptista Soberano <agostinhosoberano2@gmail.com>
2023-10-15 20:29:48 +0200

    adding db table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
             $table->string('email')->unique();
+            $table->string('telefone')->nullable();
+            $table->string('endereco')->nullable();
+            $table->string('tipo_usuario')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
