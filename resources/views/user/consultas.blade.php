ec65623fdd02bbb5794035dc622204dc0d0ea7de
Agostinho Dalte Baptista Soberano <agostinhosoberano2@gmail.com>
2023-10-20 20:31:24 +0200

    refatorando user.home.blade.php

---
+++ resources/views/user/consultas.blade.php
@@ -0,0 +1,36 @@
+<div class="page-section">
+    <div class="container">
+      <h1 class="text-center wow fadeInUp">Agende Uma Consulta</h1>
+
+      <form class="main-form">
+        <div class="row mt-5 ">
+          <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
+            <input type="text" class="form-control" placeholder="Nome Completo">
+          </div>
+          <div class="col-12 col-sm-6 py-2 wow fadeInRight">
+            <input type="text" class="form-control" placeholder="Email...">
+          </div>
+          <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
+            <input type="date" class="form-control">
+          </div>
+          <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
+            <select name="departement" id="departement" class="custom-select">
+              <option value="general">Saúde geral</option>
+              <option value="cardiology">Cardiologia</option>
+              <option value="dental">Dental</option>
+              <option value="neurology">Neurologia</option>
+              <option value="orthopaedics">Ortopedia</option>
+            </select>
+          </div>
+          <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
+            <input type="text" class="form-control" placeholder="Telefone">
+          </div>
+          <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
+            <textarea name="message" id="message" class="form-control" rows="6" placeholder="Por favor escreva uma mensagem..."></textarea>
+          </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-3 wow zoomIn">Submeter</button>
+      </form>
+    </div>
+  </div> <!-- .page-section -->
